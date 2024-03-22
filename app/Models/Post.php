3418fa3d2b859416c43ac1ea167813677f9f771f
omar <gs_zero_main@mail.ru>
2024-03-22 10:54:35 +0000

    Add migrations for database schema

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory; // данный трейт позволяет нам наполнять базу фейковыми данными
+    use SoftDeletes; // данный трейт подключает мягкое удаление данных, то есть данные не удалятся из самой таблицы
 }
