333263110360f09b7ea6090797a7d117a8c9b7a8
omar <gs_zero_main@mail.ru>
2024-03-22 12:37:42 +0000

    Clear unnecessery routes adn view files

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <Head title="Social Media Website" />
+
+    Test
+
+</template>
