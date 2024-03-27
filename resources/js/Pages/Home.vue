29818c38acf2e596594aea0188da1c3048f8cbbf
omar <gs_zero_main@mail.ru>
2024-03-27 14:48:56 +0000

    Implement user profile page UI

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,10 @@
 <script setup>
-import {Head, Link} from '@inertiajs/vue3';
+import {Head} from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({});
 
